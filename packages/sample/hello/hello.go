320f30e77c1941cff96e5dd04f3fa5a29d4d8afb
Ryan Chia <ryan_chia8@hotmail.com>
2026-07-02 08:29:58 +0000

    test

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -1,19 +1,24 @@
 package main
 
-import "os"
+import (
+	"os"
+
+	"github.com/hashicorp/go-tfe"
+)
 
 func Main(args map[string]interface{}) map[string]interface{} {
-	os.LookupEnv("TFE_TOKEN")
+	token, success := os.LookupEnv("TFE_TOKEN")
+
+	if !success {
+		return map[string]interface{}{"error": "no env"}
+	}
+
+	tfe.NewClient(&tfe.Config{
+		// BasePath: "/api/v2",
+		Token: token,
+	})
 
 	return map[string]interface{}{"error": "no env"}
-	// if !success {
-	// 	return map[string]interface{}{"error": "no env"}
-	// }
-
-	// client, err := tfe.NewClient(&tfe.Config{
-	// 	// BasePath: "/api/v2",
-	// 	Token: token,
-	// })
 
 	// if err != nil {
 	// 	return map[string]interface{}{"error": err.Error()}
