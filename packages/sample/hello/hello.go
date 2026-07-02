233b147899cabf9c90bdde40f9003ae9dc5d62b1
Ryan Chia <ryan_chia8@hotmail.com>
2026-07-02 07:12:12 +0000

    test

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"os"
-
-	tfe "github.com/hashicorp/go-tfe"
 )
 
-func Main(args map[string]interface{}) map[string]interface{} {
-	token, success := os.LookupEnv("TFE_TOKEN")
+func Main(args map[string]interface{}) map[string]string {
+	os.LookupEnv("TFE_TOKEN")
 
-	if !success {
-		return map[string]interface{}{"error": "no env"}
-	}
+	return map[string]string{"error": "no env"}
 
-	client, err := tfe.NewClient(&tfe.Config{
-		// BasePath: "/api/v2",
-		Token: token,
-	})
+	// if !success {
+	// 	return map[string]string{"error": "no env"}
+	// }
 
-	if err != nil {
-		return map[string]interface{}{"error": err.Error()}
-	}
+	// client, err := tfe.NewClient(&tfe.Config{
+	// 	// BasePath: "/api/v2",
+	// 	Token: token,
+	// })
 
-	return map[string]interface{}{
-		"token":   token[:5],
-		"version": client.RemoteAPIVersion(),
-	}
+	// if err != nil {
+	// 	return map[string]string{"error": err.Error()}
+	// }
+
+	// return map[string]string{
+	// 	"token":   token[:5],
+	// 	"version": client.RemoteAPIVersion(),
+	// }
 
 	// name, ok := args["name"].(string)
 	// if !ok {
