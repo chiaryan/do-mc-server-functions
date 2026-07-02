8e3fb8f742a1768cfe03003ab5e420df6dd28723
Ryan Chia <ryan_chia8@hotmail.com>
2026-07-02 08:02:58 +0000

    add back

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -2,37 +2,30 @@ package main
 
 import (
 	"os"
+
+	"github.com/hashicorp/go-tfe"
 )
 
 func Main(args map[string]interface{}) map[string]string {
-	os.LookupEnv("TFE_TOKEN")
+	token, success := os.LookupEnv("TFE_TOKEN")
 
-	return map[string]string{"error": "no env"}
+	// return map[string]string{"error": "no env"}
 
-	// if !success {
-	// 	return map[string]string{"error": "no env"}
-	// }
+	if !success {
+		return map[string]string{"error": "no env"}
+	}
 
-	// client, err := tfe.NewClient(&tfe.Config{
-	// 	// BasePath: "/api/v2",
-	// 	Token: token,
-	// })
+	client, err := tfe.NewClient(&tfe.Config{
+		// BasePath: "/api/v2",
+		Token: token,
+	})
 
-	// if err != nil {
-	// 	return map[string]string{"error": err.Error()}
-	// }
+	if err != nil {
+		return map[string]string{"error": err.Error()}
+	}
 
-	// return map[string]string{
-	// 	"token":   token[:5],
-	// 	"version": client.RemoteAPIVersion(),
-	// }
-
-	// name, ok := args["name"].(string)
-	// if !ok {
-	// 	name = "stranger"
-	// }
-	// dummygo.Add(1, 2, 3)
-	// msg := make(map[string]interface{})
-	// msg["body"] = "Hello " + name + "!"
-	// return msg
+	return map[string]string{
+		"token":   token[:5],
+		"version": client.RemoteAPIVersion(),
+	}
 }
