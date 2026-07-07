23258d0a41d9c1c48cd6cdebc4fe9507bfef9e20
Ryan Chia <ryan_chia8@hotmail.com>
2026-07-07 02:38:30 +0000

    add stop function

---
+++ packages/sample/stop/stop.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/hashicorp/go-tfe"
+)
+
+func Main(ctx context.Context, args map[string]interface{}) map[string]interface{} {
+	tfe_token, success := os.LookupEnv("TFE_TOKEN")
+	if !success {
+		return map[string]interface{}{
+			"body": map[string]interface{}{
+				"error": "no tfe token",
+			},
+		}
+	}
+	workspace_id, success := os.LookupEnv("WORKSPACE_ID")
+
+	if !success {
+		return map[string]interface{}{
+			"body": map[string]interface{}{
+				"error": "no workspace id",
+			},
+		}
+	}
+
+	client, err := tfe.NewClient(&tfe.Config{
+		Token: tfe_token,
+	})
+
+	if err != nil {
+		return map[string]interface{}{
+			"body": map[string]interface{}{
+				"error": err.Error(),
+			},
+		}
+	}
+
+	run, err := client.Runs.Create(ctx, tfe.RunCreateOptions{
+		Workspace:       &tfe.Workspace{ID: workspace_id},
+		AllowEmptyApply: tfe.Bool(false),
+		AutoApply:       tfe.Bool(true),
+	})
+
+	if err != nil {
+		return map[string]interface{}{
+			"body": map[string]interface{}{
+				"error creating Run": err.Error(),
+			},
+		}
+	}
+
+	return map[string]interface{}{
+		"body": map[string]interface{}{
+			"run": run.ID,
+		},
+	}
+}
